2df75180651b3f4a609a0decccf7621ce0916b06
超 <37031491+2anchao@users.noreply.github.com>
2022-04-19 15:04:11 +0800

    Update xml_dataset.py

--- nanodet/data/dataset/xml_dataset.py
+++ nanodet/data/dataset/xml_dataset.py
@@ -100,10 +100,10 @@ class XMLDataset(CocoDataset):
                 for cat in categories:
                     if category == cat["name"]:
                         cat_id = cat["id"]
-                xmin = int(_object.find("bndbox").find("xmin").text)
-                ymin = int(_object.find("bndbox").find("ymin").text)
-                xmax = int(_object.find("bndbox").find("xmax").text)
-                ymax = int(_object.find("bndbox").find("ymax").text)
+                xmin = int(eval(_object.find("bndbox").find("xmin").text))
+                ymin = int(eval(_object.find("bndbox").find("ymin").text))
+                xmax = int(eval(_object.find("bndbox").find("xmax").text))
+                ymax = int(eval(_object.find("bndbox").find("ymax").text))
                 w = xmax - xmin
                 h = ymax - ymin
                 if w < 0 or h < 0:
